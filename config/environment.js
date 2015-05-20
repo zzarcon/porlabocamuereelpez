60f5b117561bf4c99ebcfee55aab841dc10a1346
Hector Zarco <hezarco@gmail.com>
2015-05-20 19:53:18 +0200

    Sort faces

--- config/environment.js
+++ config/environment.js
@@ -9,8 +9,15 @@ module.exports = function(environment) {
     EmberENV: {
       FEATURES: {}
     },
-
-    APP: {}
+    APP: {},
+    contentSecurityPolicy: {
+      'img-src': "* 'self' 'unsafe-inline'",
+      'connect-src': "*",
+      'style-src': "* 'self' 'unsafe-inline'",
+      'report-uri': "*",
+      'script-src': "* 'self' 'unsafe-inline' 'unsafe-eval'",
+      'font-src': "* 'self' 'unsafe-inline' 'unsafe-eval'"
+    }
   };
 
   if (environment === 'development') {
