935b10dd0966b06ac0bc82ed4365c90f61739a66
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-05-22 22:48:36 +0200

    Create detail view!

--- app/components/cartoon-face.js
+++ app/components/cartoon-face.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['cartoon-face-component'],
-  classNameBindings: ['isHover', 'face.name'],
+  classNameBindings: ['isHover', 'face.name', 'mouseEvents'],
+  mouseEvents: true,
   face: null,
   isHover: false,
+  autoplayDelay: 4000,
 
   audio: function() {
     return new Audio(this.get('src'));
@@ -23,11 +25,15 @@ export default Ember.Component.extend({
   }.property('face.name', 'face.quote'),
 
   mouseEnter: function() {
+    if (!this.get('mouseEvents')) return;
+
     this.play();
     this.set('isHover', true);
   },
 
   mouseLeave: function() {
+    if (!this.get('mouseEvents')) return;
+
     this.pause();
     this.set('isHover', false);
   },
@@ -48,5 +54,17 @@ export default Ember.Component.extend({
   pause: function() {
     this.get('audio').pause();
     this.get('audio').currentTime = 0;
+  },
+
+  autoplay: function() {
+    if (this.get('mouseEvents')) return;
+
+    Ember.run.later(this, this.play, this.get('autoplayDelay'));
+  }.on('didInsertElement'),
+
+  actions: {
+    play: function() {
+      this.play();
+    }
   }
 });
