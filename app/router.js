4fcb48d40ae1c32e78dac769e20bd2a3e5ad4a8f
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-05-25 14:11:17 +0200

    Add Google Analytics

--- app/router.js
+++ app/router.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+
+  /**
+   * Send info to Google Analytics
+   * @return {Object}
+   */
+  notifyGoogleAnalytics: function() {
+    return ga('send', 'pageview', {
+      page: this.get('url'),
+      title: this.get('url')
+    });
+  }.on('didTransition')
 });
 
 export default Router.map(function() {
